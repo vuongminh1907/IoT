0005003954fb7be89681a7181898fc5786b81d94
vuongminh1907 <vuongminhkhoi4@gmail.com>
2024-12-06 00:35:43 +0700

    init

--- test.py
+++ test.py
@@ -46,8 +46,39 @@ def is_v_sign(landmarks, image_width, image_height):
 
     return is_v
 
+# Hàm kiểm tra cử chỉ "Like"
+def is_like_sign(landmarks, image_width, image_height):
+    is_like = False
+    if landmarks[3].y < min(landmarks[5].y, landmarks[9].y, landmarks[13].y, landmarks[17].y):
+        is_like = True
+    #nếu landmark[3].x không nằm giữa landmark[5].x và landmark[6].x
+    if landmarks[3].x > landmarks[5].x and landmarks[3].x < landmarks[6].x:
+        is_like = True
+    else:
+        is_like = False
+    return is_like
+
+def is_dislike_sign(landmarks, image_width, image_height):
+    is_dislike = False
+
+    if landmarks[3].y > max(landmarks[5].y, landmarks[9].y, landmarks[13].y, landmarks[17].y):
+        is_dislike = True
+    else:
+        is_dislike = False
+
+    if landmarks[2].x < landmarks[10].x and landmarks[2].x > landmarks[0].x:
+        is_dislike = is_dislike and True
+    else:
+        is_dislike = False
+    return is_dislike
+
+
 # Khởi động video
 cap = cv2.VideoCapture(0)
+
+bool_v_sign = False
+bool_adjust_volume = False
+count_like_and_dislike = 0
 
 # Khởi tạo Mediapipe Hands
 with mp_hands.Hands(
@@ -70,6 +101,9 @@ with mp_hands.Hands(
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+        if bool_v_sign:
+            cv2.putText(image, "On Mode Adjust Volume", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 # Vẽ bàn tay và keypoints
@@ -81,13 +115,37 @@ with mp_hands.Hands(
 
                 # Kiểm tra cử chỉ "V"
                 if is_v_sign(landmarks, w, h):
-                    cv2.putText(image, "V-Sign Detected", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    if not bool_v_sign:
+                        cv2.putText(image, "V-Sign Detected", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    bool_v_sign = True
+                    bool_adjust_volume = True
 
+                if bool_adjust_volume:
+                    print("Adjust Volume")
+
+                if is_like_sign(landmarks, w, h):
+                    count_like_and_dislike += 1
+                if is_dislike_sign(landmarks, w, h):
+                    count_like_and_dislike -= 1
+
+                if count_like_and_dislike > 10:
+                    cv2.putText(image, "Like Sign Detected", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    count_like_and_dislike = 0
+                    bool_v_sign = False
+
+                if count_like_and_dislike < -10:
+                    cv2.putText(image, "Dislike Sign Detected", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    count_like_and_dislike = 0
+                    bool_v_sign = False
         # Hiển thị kết quả
         cv2.imshow('Hand Gesture Recognition', image)
 
         # Nhấn 'q' để thoát
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        #nếu tắt cửa sổ thì thoát
+        if cv2.getWindowProperty('Hand Gesture Recognition', cv2.WND_PROP_VISIBLE) < 1:
             break
 
 # Giải phóng tài nguyên
